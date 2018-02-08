190466702dec3a02fc72e0a2fad2b15bc6765b01
Lars Ivar Hatledal <laht@ntnu.no>
2018-02-08 10:34:52 +0100

    remoteFMU equals on guid

--- java/gRPC-FMU/web/src/main/kotlin/no/mechatronics/sfi/grpc_fmu/web/fmu/FmuBean.kt
+++ java/gRPC-FMU/web/src/main/kotlin/no/mechatronics/sfi/grpc_fmu/web/fmu/FmuBean.kt
@@ -106,8 +106,25 @@ class RemoteFmu(
         protoDefinition = ProtoDefinitions(definitions, service)
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RemoteFmu
+
+        if (guid != other.guid) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return guid.hashCode()
+    }
+
     override fun toString(): String {
         return "RemoteFmu(guid='$guid', address='$address')"
     }
+
+
 
 }
