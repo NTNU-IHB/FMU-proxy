979d42d9c96b0cde1c10a78ba6849df70f0d26d3
Lars Ivar Hatledal <laht@ntnu.no>
2018-07-16 23:17:00 +0200

    add command line parser to fmu_proxy exec

--- cpp/FMU-proxy/tool/fmu_proxy.cpp
+++ cpp/FMU-proxy/tool/fmu_proxy.cpp
@@ -22,16 +22,81 @@
  * THE SOFTWARE.
  */
 
+#include <iostream>
 #include <fmuproxy/fmi/Fmu.hpp>
 #include <fmuproxy/thrift/server/ThriftServer.hpp>
+#include "boost/program_options.hpp"
 
 using namespace std;
 using namespace fmuproxy::fmi;
 using namespace fmuproxy::thrift::server;
 
-int main(int argc, char* argv[]) {
+namespace {
+
+    const int SUCCESS = 0;
+    const int ERROR_IN_COMMAND_LINE = 1;
+    const int ERROR_UNHANDLED_EXCEPTION = 2;
+
+    void wait_for_input() {
+        do {
+            cout << '\n' << "Press a key to continue...\n";
+        } while (cin.get() != '\n');
+        cout << "Done." << endl;
+    }
+
+    int run_application(string &fmu_path) {
+
+        Fmu fmu(fmu_path);
+
+        wait_for_input();
+
+        return 0;
+
+    }
+
+}
 
 
-    return 0;
+int main(int argc, char** argv) {
+
+    try {
+
+        namespace po = boost::program_options;
+
+        po::options_description desc("Options");
+        desc.add_options()
+                ("help,h", "Print this help message and quits.")
+                ("fmu,f", po::value<string>()->required(), "Path to FMU");
+
+        po::variables_map vm;
+        try {
+
+            po::store(po::parse_command_line(argc, argv, desc), vm);
+
+            if ( vm.count("help") ) {
+                cout << "FMU-proxy" << endl << desc << endl;
+                return SUCCESS;
+            }
+
+            po::notify(vm);
+
+        } catch(po::error& e) {
+            std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+            std::cerr << desc << std::endl;
+            return ERROR_IN_COMMAND_LINE;
+        }
+
+        if (vm.count("fmu")) {
+            string fmu_path = vm["fmu"].as<string>();
+            return run_application(fmu_path);
+        }
+
+
+    } catch(std::exception& e) {
+        std::cerr << "Unhandled Exception reached the top of main: " << e.what() << ", application will now exit" << std::endl;
+        return ERROR_UNHANDLED_EXCEPTION;
+    }
+
+    return SUCCESS;
 
 }
