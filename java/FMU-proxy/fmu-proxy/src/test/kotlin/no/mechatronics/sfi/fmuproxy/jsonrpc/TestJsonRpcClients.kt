af3d1a3505a6740d6dd29663b87134a79511e21b
Lars Ivar Hatledal <laht@hials.no>
2018-07-17 22:29:58 +0200

    update yaj-rpc

--- java/FMU-proxy/fmu-proxy/src/test/kotlin/no/mechatronics/sfi/fmuproxy/jsonrpc/TestJsonRpcClients.kt
+++ java/FMU-proxy/fmu-proxy/src/test/kotlin/no/mechatronics/sfi/fmuproxy/jsonrpc/TestJsonRpcClients.kt
@@ -1,7 +1,6 @@
 package no.mechatronics.sfi.fmuproxy.jsonrpc
 
 import info.laht.yajrpc.RpcHandler
-import info.laht.yajrpc.net.RpcClient
 import info.laht.yajrpc.net.http.RpcHttpClient
 import info.laht.yajrpc.net.tcp.RpcTcpClient
 import info.laht.yajrpc.net.ws.RpcWebSocketClient
@@ -19,13 +18,10 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
-import org.junit.jupiter.api.condition.EnabledOnOs
-import org.junit.jupiter.api.condition.OS
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
 
-@EnabledOnOs(OS.WINDOWS)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @EnabledIfEnvironmentVariable(named = "TEST_FMUs", matches = ".*")
 class TestJsonRpcClients {
@@ -43,7 +39,7 @@ class TestJsonRpcClients {
 
     private var proxy: FmuProxy
     private val fmu = Fmu.from(File(TestUtils.getTEST_FMUs(),
-            "FMI_2.0/CoSimulation/win64/FMUSDK/2.0.4/BouncingBall/bouncingBall.fmu"))
+            "FMI_2.0/CoSimulation/${TestUtils.getOs()}/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu"))
     private var modelDescription: CommonModelDescription = fmu.modelDescription
 
     init {
@@ -87,13 +83,13 @@ class TestJsonRpcClients {
                     instance.init()
                     Assertions.assertEquals(FmiStatus.OK, instance.lastStatus)
 
-                    val h = client.modelDescription.modelVariables
-                            .getByName("h").asRealVariable()
+                    val temp = client.modelDescription.modelVariables
+                            .getByName("Temperature_Room").asRealVariable()
 
-                    val dt = 1.0/100
-                    val stop = 10.0
+                    val dt = 1E-4
+                    val stop = 1.0
                     runInstance(instance, dt, stop) {
-                        h.read()
+                        temp.read()
                     }.also { LOG.info("Duration: ${it}ms") }
 
                 }
