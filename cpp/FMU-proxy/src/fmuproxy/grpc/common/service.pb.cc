ca6968b882f43f9053b3ae9e2a2fcf41cdd1ffaf
Lars Ivar Hatledal <laht@ntnu.no>
2018-08-02 10:48:47 +0200

    Update protoc generated files

--- cpp/FMU-proxy/src/fmuproxy/grpc/common/service.pb.cc
+++ cpp/FMU-proxy/src/fmuproxy/grpc/common/service.pb.cc
@@ -3,6 +3,13 @@
 
 #include <fmuproxy/grpc/common/service.pb.h>
 
+#include <algorithm>
+
+#include <google/protobuf/stubs/common.h>
+#include <google/protobuf/stubs/port.h>
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/wire_format_lite_inl.h>
+#include <google/protobuf/descriptor.h>
 #include <google/protobuf/generated_message_reflection.h>
 #include <google/protobuf/reflection_ops.h>
 #include <google/protobuf/wire_format.h>
