0373616ae8e9074b4e31d10b67accf72f5e234fc
Lars Ivar Hatledal <laht@ntnu.no>
2018-08-10 09:27:44 +0200

    add RunServlet class

---
+++ java/FMU-proxy/fmu-proxy/src/test/kotlin/no/mechatronics/sfi/fmuproxy/thrift/RunServlet.kt
@@ -0,0 +1,32 @@
+package no.mechatronics.sfi.fmuproxy.thrift
+
+import no.mechatronics.sfi.fmi4j.importer.Fmu
+import no.mechatronics.sfi.fmuproxy.TestUtils
+import java.io.File
+import java.util.*
+
+object RunServlet {
+
+    private val fmuPath = File(TestUtils.getTEST_FMUs(),
+            "FMI_2.0/CoSimulation/${TestUtils.getOs()}" +
+                    "/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu")
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        Fmu.from(fmuPath).use { fmu ->
+            val server = ThriftFmuServlet(fmu).apply {
+                start(9090)
+            }
+
+            println("Press any key to quit..")
+            Scanner(System.`in`).use {
+                if (it.hasNext()) {
+                    server.close()
+                }
+            }
+        }
+
+    }
+
+}
