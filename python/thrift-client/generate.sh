fc7441182dd2df0a3f37470aa1fdf734b8ad4a25
Lars Ivar Hatledal <laht@ntnu.no>
2018-09-18 00:05:19 +0200

    update generate.sh

--- python/thrift-client/generate.sh
+++ python/thrift-client/generate.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 thrift -r --gen py ../../rpc-definitions/thrift/service.thrift
+
+rm -r definitions
+rm -r service
+
 mv gen-py/definitions definitions
 mv gen-py/service service
+
 rm -r gen-py
