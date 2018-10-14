2ff1e1b1ecbbbf80a14d7a2a667f8e0093682d7f
Lars Ivar Hatledal <laht@hials.no>
2018-10-14 12:12:24 +0200

    refactor helper

--- cpp/FMU-proxy/src/fmuproxy/heartbeat/heartbeat_helper.hpp
+++ cpp/FMU-proxy/src/fmuproxy/heartbeat/heartbeat_helper.hpp
@@ -31,52 +31,22 @@
 #include <cctype>
 #include <algorithm>
 #include <vector>
-#include <boost/uuid/uuid_io.hpp>
+
+#include <curl/curl.h>
 #include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/algorithm/string/join.hpp>
-#include <curl/curl.h>
+
 #include <fmuproxy/heartbeat/RemoteAddress.hpp>
 
-using namespace std;
 using namespace boost::uuids;
 
-namespace fmuproxy::heartbeat {
-
-    const string generate_uuid() {
-        random_generator generator;
-        uuid uuid = generator();
-        return to_string(uuid);
-    }
-
-    //https://stackoverflow.com/questions/7724448/simple-json-string-escape-for-c
-    const string toJSON(const std::string &s) {
-        std::ostringstream o;
-        for (auto c = s.cbegin(); c != s.cend(); c++) {
-            if (*c == '"' || *c == '\\' || ('\x00' <= *c && *c <= '\x1f')) {
-                o << "\\u"
-                  << std::hex << std::setw(4) << std::setfill('0') << (int)*c;
-            } else {
-                o << *c;
-            }
-        }
-        return o.str();
-    }
-
-    const string toJSON(vector<string> v) {
-        return "[" + boost::algorithm::join(v, ", ") + "]";
-    }
-
-    // https://stackoverflow.com/questions/44994203/how-to-get-the-http-response-string-using-curl-in-c
-    const size_t write_callback(char *contents, size_t size, size_t nmemb, void *userp) {
-        ((std::string*)userp)->append(contents, size * nmemb);
-        return size * nmemb;
-    }
-
+namespace {
 
     //https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
     // trim from start (in place)
-    static inline void ltrim(string &s) {
+    inline void ltrim(std::string &s) {
         s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
             return !std::isspace(ch);
         }));
@@ -84,7 +54,7 @@ namespace fmuproxy::heartbeat {
 
     //https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
     // trim from end (in place)
-    static inline void rtrim(string &s) {
+    inline void rtrim(std::string &s) {
         s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
             return !std::isspace(ch);
         }).base(), s.end());
@@ -92,14 +62,49 @@ namespace fmuproxy::heartbeat {
 
     //https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
     // trim from both ends (in place)
-    static inline void trim(string &s) {
+    inline void trim(std::string &s) {
         ltrim(s);
         rtrim(s);
     }
 
-    static inline CURLcode post(const RemoteAddress &remote, CURL *curl, string &response, const string &ctx, const string &data) {
+}
 
-        const string url = "http://" + remote.host + ":" + to_string(remote.port) + "/fmu-proxy/" + ctx;
+namespace fmuproxy::heartbeat {
+
+    const std::string generate_uuid() {
+        random_generator generator;
+        uuid uuid = generator();
+        return to_string(uuid);
+    }
+
+    //https://stackoverflow.com/questions/7724448/simple-json-string-escape-for-c
+    const std::string toJSON(const std::string &s) {
+        std::ostringstream o;
+        for (auto c = s.cbegin(); c != s.cend(); c++) {
+            if (*c == '"' || *c == '\\' || ('\x00' <= *c && *c <= '\x1f')) {
+                o << "\\u"
+                  << std::hex << std::setw(4) << std::setfill('0') << (int) *c;
+            } else {
+                o << *c;
+            }
+        }
+        return o.str();
+    }
+
+    const std::string toJSON(std::vector<std::string> v) {
+        return "[" + boost::algorithm::join(v, ", ") + "]";
+    }
+
+    // https://stackoverflow.com/questions/44994203/how-to-get-the-http-response-string-using-curl-in-c
+    const size_t write_callback(char *contents, size_t size, size_t nmemb, void *userp) {
+        ((std::string *) userp)->append(contents, size * nmemb);
+        return size * nmemb;
+    }
+
+    static inline CURLcode post(const RemoteAddress &remote, CURL *curl, std::string &response, const std::string &ctx,
+                         const std::string &data) {
+
+        const std::string url = "http://" + remote.host + ":" + std::to_string(remote.port) + "/fmu-proxy/" + ctx;
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
 
