e5dd450fcda9e9099d7566011108fafac10e499c
Lars Ivar Hatledal <laht@ntnu.no>
2019-02-14 13:18:34 +0100

    add conan support. closes #68

---
+++ cpp/FMU-proxy/conanfile.py
@@ -0,0 +1,37 @@
+
+from conans import ConanFile, CMake
+
+
+class FMI4cppConan(ConanFile):
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "virtualrunenv"
+    requires = (
+        "boost/1.66.0@conan/stable",
+        "OpenSSL/1.0.2o@conan/stable",
+        "libcurl/7.61.1@bincrafters/stable",
+        "libzip/1.5.1@bincrafters/stable",
+        "spdlog/1.3.1@bincrafters/stable",
+        "jsonformoderncpp/3.5.0@vthiery/stable",
+        "libevent/2.0.22@bincrafters/stable"
+    )
+    options = {
+        "thrift": [True, False],
+        "grpc": [True, False]
+    }
+    default_options = (
+        "thrift=True",
+        "grpc=True",
+        "boost:shared=True",
+        "libzip:shared=True",
+        "libcurl:shared=True"
+    )
+
+    def configure(self):
+        pass
+
+    def requirements(self):
+        if self.options.thrift:
+            self.requires("thrift/0.12.0@helmesjo/stable")
+        if self.options.grpc:
+            self.requires("grpc/1.17.2@inexorgame/stable")
---
+++ cpp/FMU-proxy/run_conan_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+conan remote add helmesjo "https://api.bintray.com/conan/helmesjo/public-conan" --f
+conan remote add inexorgame "https://api.bintray.com/conan/inexorgame/inexor-conan" --f
+
+conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug -o thrift=False --build=missing
+conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release --build=missing
