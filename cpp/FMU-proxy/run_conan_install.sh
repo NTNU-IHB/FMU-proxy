c851c224db35353b3777f483acd5bc68d8cb7eb6
Lars Ivar Hatledal <laht@ntnu.no>
2019-02-14 13:47:26 +0100

    update conan install script

--- cpp/FMU-proxy/run_conan_install.sh
+++ cpp/FMU-proxy/run_conan_install.sh
@@ -3,5 +3,5 @@
 conan remote add helmesjo "https://api.bintray.com/conan/helmesjo/public-conan" --f
 conan remote add inexorgame "https://api.bintray.com/conan/inexorgame/inexor-conan" --f
 
-conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug -o thrift=False --build=missing
-conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release --build=missing
+conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug -o thrift=False -o grpc=True --build=missing
+conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release -o grpc=True --build=missing
