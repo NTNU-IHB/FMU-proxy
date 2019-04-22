7dc838893ba2492aef19c09e346da0be4a9042f6
Lars Ivar Hatledal <laht@ntnu.no>
2019-04-22 10:08:27 +0200

    add typedef and use 0.12.0 compiler

--- cpp/FMU-proxy/generate_thrift.sh
+++ cpp/FMU-proxy/generate_thrift.sh
@@ -11,7 +11,7 @@ mv gen/FmuService.cpp gen/fmu_service.cpp
 sed -i 's/"service_types.h"/<fmuproxy\/thrift\/common\/service_types.h>/' gen/service_types.cpp
 sed -i 's/"FmuService.h"/<fmuproxy\/thrift\/common\/fmu_service.h>/' gen/fmu_service.cpp
 
-sed -i 's/}} \/\/ namespace/typedef FmuServiceIf fmu_service_if;\ntypedef FmuServiceClient fmu_service_client;\ntypedef FmuServiceProcessor fmu_service_processor;\n}} \/\/ namespace/' gen/fmu_service.h
+sed -i 's/}} \/\/ namespace/typedef FmuServiceIf fmu_service_if;\ntypedef FmuServiceClient fmu_service_client;\ntypedef FmuServiceProcessor fmu_service_processor;\ntypedef FmuServiceConcurrentClient fmu_service_concurrent_client;\n}} \/\/ namespace/' gen/fmu_service.h
 
 mv gen/service_types.h include/fmuproxy/thrift/common/
 mv gen/fmu_service.h include/fmuproxy/thrift/common/
